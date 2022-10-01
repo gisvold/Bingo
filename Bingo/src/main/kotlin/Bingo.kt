e44d51b6c96309e3ecae08d7c60491756b44ff5c
Aleks Gisvold <agi@aztek.no>
2022-10-01 18:42:14 +0200

    Made the draw announcement look better

--- Bingo/src/main/kotlin/Bingo.kt
+++ Bingo/src/main/kotlin/Bingo.kt
@@ -12,18 +12,18 @@ fun main(args: Array<String>) {
     println("I would like to extend a heartfelt welcome to you all!")
 
     Thread.sleep(sleepTime)
-    println("Bingo number #1 of the day is:" + drawBingoNumber(alreadyDrawn))
+    println("Bingo number #1 of the day is: " + drawBingoNumber(alreadyDrawn))
 
     Thread.sleep(sleepTime)
-    println("Bingo number #2 of the day is:" + drawBingoNumber(alreadyDrawn))
+    println("Bingo number #2 of the day is: " + drawBingoNumber(alreadyDrawn))
 
     Thread.sleep(sleepTime)
-    println("Bingo number #3 of the day is:" + drawBingoNumber(alreadyDrawn))
+    println("Bingo number #3 of the day is: " + drawBingoNumber(alreadyDrawn))
 
 }
 
 fun checkAlreadyDrawn(drawnNumber: Int): Boolean {
-    return alreadyDrawn.contains(drawnNumber);
+    return alreadyDrawn.contains(drawnNumber)
 }
 
 fun drawBingoNumber(alreadyDrawn: ArrayList<Int>): Int {
